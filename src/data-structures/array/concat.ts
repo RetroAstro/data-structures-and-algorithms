2d5dd5ad751a973b6f865f3b978c860b029a80f9
RetroAstro <452461114@qq.com>
2019-09-30 22:00:49 +0800

    style

--- src/data-structures/array/concat.ts
+++ src/data-structures/array/concat.ts
@@ -3,7 +3,7 @@ import { Container } from './container'
 export class concatContainer extends Container {
   concat(...args: any[]) {
     let result = [...this._array]
-    for (var i = 0; i < args.length; i++) {
+    for (let i = 0; i < args.length; i++) {
       result = [
         ...result,
         ...(Array.isArray(args[i]) ? args[i] : [args[i]])
--- src/data-structures/array/splice.ts
+++ src/data-structures/array/splice.ts
@@ -16,27 +16,27 @@ export class spliceContainer extends Container {
     if (deleteCount < 0) {
       deleteCount = 0
     }
-    var newValue = this.makeNewValue(start, deleteCount, args)
-    var deleteValue = this.makeDeleteValue(start, deleteCount)
+    let newValue = this.makeNewValue(start, deleteCount, args)
+    let deleteValue = this.makeDeleteValue(start, deleteCount)
     return [newValue, deleteValue]
   }
   makeNewValue(start: number, deleteCount: number, args: any[]) {
-    var result = []
-    for (var i = 0; i < start; i++) {
+    let result = []
+    for (let i = 0; i < start; i++) {
       result[i] = this._array[i]
     }
-    for (var i = 0; i < args.length; i++) {
+    for (let i = 0; i < args.length; i++) {
       result.push(args[i])
     }
-    for (var i = start + deleteCount; i < this._array.length; i++) {
+    for (let i = start + deleteCount; i < this._array.length; i++) {
       result.push(this._array[i])
     }
     return result
   }
   makeDeleteValue(start: number, deleteCount: number) {
-    var result = []
-    var limit = (start + deleteCount) > this._array.length ? this._array.length : (start + deleteCount)
-    for (var i = start; i < limit; i++) {
+    let result = []
+    let limit = (start + deleteCount) > this._array.length ? this._array.length : (start + deleteCount)
+    for (let i = start; i < limit; i++) {
       result.push(this._array[i])
     }
     return result
