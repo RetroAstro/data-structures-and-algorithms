15defc2576995a11514cd8cd79f3a824b63cda9b
RetroAstro <452461114@qq.com>
2019-10-25 00:05:10 +0800

    circular queue

---
+++ src/data-structures/queue/circular-queue.ts
@@ -0,0 +1,49 @@
+export class CircularQueue<T> {
+  private head: number = 0
+  private tail: number = 0
+  private items: { [key: number]: T } = {}
+
+  constructor(private capacity: number) {}
+
+  enqueue(data: T) {
+    if (
+      (this.tail + 1) % this.capacity === this.head
+    ) {
+      return false
+    }
+    this.items[this.tail] = data
+    this.tail = (this.tail + 1) % this.capacity
+    return true
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    let result = this.items[this.head]
+    delete this.items[this.head]
+    this.head = (this.head + 1) % this.capacity
+    return result
+  }
+
+  peek() {
+    return this.items[this.head]
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  size() {
+    if (this.tail >= this.head) {
+      return this.tail - this.head
+    }
+    return (this.capacity - this.head) + this.tail
+  }
+
+  clear() {
+    this.head = 0
+    this.tail = 0
+    this.items = {}
+  }
+}
---
+++ tests/data-structures/queue/circular-queue.spec.ts
@@ -0,0 +1,130 @@
+import { expect } from 'chai'
+import { CircularQueue } from '../../../src/data-structures/queue/circular-queue'
+
+describe('circular-queue', () => {
+  let queue: CircularQueue<number>
+  
+  beforeEach(() => {
+    queue = new CircularQueue<number>(3)
+  })
+
+  it('start empty', () => {
+    expect(queue.size()).to.equal(0)
+    expect(queue.isEmpty()).to.equal(true)
+  })
+
+  it('enqueue data', () => {
+    expect(queue.enqueue(1)).to.be.true
+    expect(queue.size()).to.equal(1)
+    expect(queue.enqueue(2)).to.be.true
+    expect(queue.size()).to.equal(2)
+    expect(queue.enqueue(3)).to.be.false
+    expect(queue.size()).to.equal(2)
+    expect(queue.isEmpty()).to.equal(false)
+  })
+
+  it('dequeue data', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    expect(queue.enqueue(3)).to.be.false
+
+    expect(queue.dequeue()).to.equal(1)
+    expect(queue.dequeue()).to.equal(2)
+    expect(queue.dequeue()).to.equal(undefined)
+  })
+
+  it('implement FIFO logic', () => {
+    queue.enqueue(1)
+    expect(queue.peek()).to.equal(1)
+    queue.enqueue(2)
+    expect(queue.peek()).to.equal(1)
+
+    expect(queue.dequeue()).to.equal(1)
+    expect(queue.dequeue()).to.equal(2)
+    expect(queue.dequeue()).to.equal(undefined)
+  })
+
+  it('allow to peek at the front data in the queue without dequeuing it', () => {
+    expect(queue.peek()).to.equal(undefined)
+
+    queue.enqueue(1)
+    expect(queue.peek()).to.equal(1)
+
+    queue.enqueue(2)
+    expect(queue.peek()).to.equal(1)
+
+    queue.dequeue()
+    expect(queue.peek()).to.equal(2)
+  })
+
+  it('return the correct size', () => {
+    expect(queue.size()).to.equal(0)
+    queue.enqueue(1)
+    expect(queue.size()).to.equal(1)
+    queue.enqueue(2)
+    expect(queue.size()).to.equal(2)
+
+    queue.clear()
+    expect(queue.isEmpty()).to.equal(true)
+
+    queue.enqueue(1)
+    expect(queue.size()).to.equal(1)
+    queue.enqueue(2)
+    expect(queue.size()).to.equal(2)
+
+    queue.dequeue()
+    expect(queue.size()).to.equal(1)
+    queue.dequeue()
+    expect(queue.size()).to.equal(0)
+    
+    queue.enqueue(3)
+    expect(queue.size()).to.equal(1)
+    queue.dequeue()
+    expect(queue.size()).to.equal(0)
+    queue.enqueue(4)
+    expect(queue.size()).to.equal(1)
+    queue.dequeue()
+    expect(queue.size()).to.equal(0)
+    queue.enqueue(5)
+    expect(queue.size()).to.equal(1)
+    queue.enqueue(6)
+    expect(queue.size()).to.equal(2)
+    queue.dequeue()
+    queue.dequeue()
+    expect(queue.size()).to.equal(0)
+  })
+
+  it('return if it is empty', () => {
+    expect(queue.isEmpty()).to.equal(true)
+    queue.enqueue(1)
+    expect(queue.isEmpty()).to.equal(false)
+    queue.enqueue(2)
+    expect(queue.isEmpty()).to.equal(false)
+
+    queue.clear()
+    expect(queue.isEmpty()).to.equal(true)
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    expect(queue.isEmpty()).to.equal(false)
+
+    queue.dequeue()
+    expect(queue.isEmpty()).to.equal(false)
+    queue.dequeue()
+    expect(queue.isEmpty()).to.equal(true)
+    queue.dequeue()
+    expect(queue.isEmpty()).to.equal(true)
+  })
+
+  it('clear the queue', () => {
+    queue.clear()
+    expect(queue.isEmpty()).to.equal(true)
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    expect(queue.isEmpty()).to.equal(false)
+
+    queue.clear()
+    expect(queue.isEmpty()).to.equal(true)
+  })
+})
