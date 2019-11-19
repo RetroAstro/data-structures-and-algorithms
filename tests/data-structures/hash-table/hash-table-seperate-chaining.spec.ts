53ab794e2885956ab5b083291f14f8f6977ecf43
RetroAstro <452461114@qq.com>
2019-11-19 16:51:03 +0800

    fix

--- tests/data-structures/hash-table/hash-table-seperate-chaining.spec.ts
+++ tests/data-structures/hash-table/hash-table-seperate-chaining.spec.ts
@@ -1,6 +1,1 @@
-import { expect } from 'chai'
-import { HashTableSeperateChaining } from '../../../src/data-structures/hash-table/hash-table-seperate-chaining'
 
-describe('hash-table-seperate-chaining', () => {
-
-})
