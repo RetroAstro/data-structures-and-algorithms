b5401134e8acb381cfd1902c417059467ad03310
RetroAstro <452461114@qq.com>
2019-11-24 16:51:37 +0800

    update

--- src/data-structures/binary-tree/binary-tree-traverse.ts
+++ src/data-structures/binary-tree/binary-tree-traverse.ts
@@ -1,21 +1,46 @@
 import { Node } from './node'
+import { SequenceQueue } from '../queue/sequence-queue'
 
 export class BinaryTreeTraverse<T> {
-  constructor(private tree: Node<T>) {}
+  queue: SequenceQueue<Node<T>>
 
-  preOrder() {
-
+  constructor(public tree: Node<T>) {
+    this.queue = new SequenceQueue<Node<T>>()
   }
 
-  inOrder() {
-
+  preOrder(res: T[], node: Node<T> = this.tree) {
+    if (node == null) return
+    res.push(node.data)
+    this.preOrder(res, node.left)
+    this.preOrder(res, node.right)
   }
 
-  postOrder() {
-
+  inOrder(res: T[], node: Node<T> = this.tree) {
+    if (node == null) return
+    this.inOrder(res, node.left)
+    res.push(node.data)
+    this.inOrder(res, node.right)
   }
-  
-  levelOrder() {
 
+  postOrder(res: T[], node: Node<T> = this.tree) {
+    if (node == null) return
+    this.postOrder(res, node.left)
+    this.postOrder(res, node.right)
+    res.push(node.data)
+  }
+
+  levelOrder(res: T[], node: Node<T> = this.tree) {
+    if (node == null) return
+    this.queue.enqueue(node)
+    while (!this.queue.isEmpty()) {
+      let temp = this.queue.dequeue()
+      res.push(temp.data)
+      if (temp.left) {
+        this.queue.enqueue(temp.left)
+      }
+      if (temp.right) {
+        this.queue.enqueue(temp.right)
+      }
+    }
   }
 }
---
+++ tests/data-structures/binary-tree/binary-tree-traverse.spec.ts
@@ -0,0 +1,62 @@
+import { expect } from 'chai'
+import { Node } from '../../../src/data-structures/binary-tree/node'
+import { BinaryTreeTraverse } from '../../../src/data-structures/binary-tree/binary-tree-traverse'
+
+function createBinaryTree<T>(array: T[], index: number = 0) {
+  let treeNode: Node<T> = null
+  if (index < array.length) {
+    treeNode = new Node<T>(array[index])
+    treeNode.left = createBinaryTree(array, 2 * index + 1)
+    treeNode.right = createBinaryTree(array, 2 * index + 2)
+  }
+  return treeNode
+}
+
+function createInstance<T>(array: T[]) {
+  let tree = createBinaryTree(array)
+  return new BinaryTreeTraverse<T>(tree)
+}
+
+describe('binary-tree-traverse', () => {
+  it('start empty', () => {
+    let instance = createInstance([])
+    let res1: any = [], res2: any = [], res3: any = [], res4: any = []
+    instance.preOrder(res1)
+    instance.inOrder(res2)
+    instance.postOrder(res3)
+    instance.levelOrder(res4)
+    expect(instance.tree).to.be.null
+    expect(res1).to.deep.equal([])
+    expect(res2).to.deep.equal([])
+    expect(res3).to.deep.equal([])
+    expect(res4).to.deep.equal([])
+  })
+
+  it('pre-order traverse', () => {
+    let result: string[] = []
+    let instance = createInstance(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    instance.preOrder(result)
+    expect(result).to.deep.equal(['A', 'B', 'D', 'E', 'C', 'F', 'G'])
+  })
+
+  it('in-order traverse', () => {
+    let result: string[] = []
+    let instance = createInstance(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    instance.inOrder(result)
+    expect(result).to.deep.equal(['D', 'B', 'E', 'A', 'F', 'C', 'G'])
+  })
+
+  it('post-order traverse', () => {
+    let result: string[] = []
+    let instance = createInstance(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    instance.postOrder(result)
+    expect(result).to.deep.equal(['D', 'E', 'B', 'F', 'G', 'C', 'A'])
+  })
+
+  it('level-order traverse', () => {
+    let result: string[] = []
+    let instance = createInstance(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    instance.levelOrder(result)
+    expect(result).to.deep.equal(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+  })
+})
