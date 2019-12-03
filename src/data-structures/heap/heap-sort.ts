f7e0f35aa5a4cfb80237c533e49096fc31259483
RetroAstro <452461114@qq.com>
2019-12-03 17:59:23 +0800

    heap-sort

--- src/data-structures/heap/heap-sort.ts
+++ src/data-structures/heap/heap-sort.ts
@@ -1,1 +1,34 @@
+import { swap } from '../../algorithms/sorting/swap'
 
+export function heapSort(array: number[]) {
+  buildMaxHeap(array)
+  let size = array.length
+  while (size > 1) {
+    swap(array, 0, --size)
+    heapify(array, 0, size)
+  }
+  return array
+}
+
+function buildMaxHeap(array: number[]) {
+  let maxIndex = Math.floor(array.length / 2)
+  for (let i = maxIndex; i >= 0; i--) {
+    heapify(array, i, array.length)
+  }
+}
+
+function heapify(array: number[], index: number, size: number) {
+  let temp = index
+  let left = 2 * index + 1
+  let right = 2 * index + 2
+  if (left < size && array[temp] < array[left]) {
+    temp = left
+  }
+  if (right < size && array[temp] < array[right]) {
+    temp = right
+  }
+  if (temp !== index) {
+    swap(array, temp, index)
+    heapify(array, temp, size)
+  }
+}
---
+++ tests/data-structures/heap/heap-sort.spec.ts
@@ -0,0 +1,4 @@
+import { heapSort } from '../../../src/data-structures/heap/heap-sort'
+import { testSortAlgorithm } from '../../algorithms/sorting/sort-algorithm-tests'
+
+testSortAlgorithm(heapSort, 'heap-sort')
