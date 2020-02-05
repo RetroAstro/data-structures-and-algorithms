9dc2af33e1ff5fd2c0871ce7994afa83b614a45c
RetroAstro <452461114@qq.com>
2020-02-05 22:13:39 +0800

    style

--- src/data-structures/recursion/permutation.ts
+++ src/data-structures/recursion/permutation.ts
@@ -5,15 +5,13 @@ export function permutation(nums: number[]) {
   
   let result: number[][] = [] 
 
-  function recursive(fixed: number[], left: number[]) {
-    if (left.length === 1) {
-      return [...fixed, ...left]
+  function recursive(fixed: number[], rest: number[]) {
+    if (rest.length === 1) {
+      return [...fixed, ...rest]
     }
-    return left.map(num => {
-      let temp = [...fixed]
-      temp[temp.length] = num
-      
-      let mids = recursive(temp, left.filter(item => item !== num))
+    return rest.map(num => {
+      let temp = [...fixed, num]
+      let mids = recursive(temp, rest.filter(item => item !== num))
 
       if (mids.length === nums.length) {
         result.push(mids as number[])
