973a949b9a1956e7fc111cd543f46115097d56c1
RetroAstro <452461114@qq.com>
2020-02-13 18:37:59 +0800

    fix

--- tests/data-structures/heap/topk-largest-elements-in-a-collection.spec.ts
+++ tests/data-structures/heap/topk-largest-elements-in-a-collection.spec.ts
@@ -1,28 +1,28 @@
 import { expect } from 'chai'
 import { topKLargest } from '../../../src/data-structures/heap/topk-largest-elements-in-a-collection'
-import { heapSort } from '../../../src/data-structures/heap/heap-sort'
+import { quickSort } from '../../../src/algorithms/sorting/quick-sort'
 
 describe('topk-largest-elements-in-a-collection', () => {
   it('should return topk largest elements in a collection when giving arrays', () => {
     let collection = new topKLargest<number>([33, 16, 13, 50], 3)
-    expect(heapSort(collection.topK)).to.deep.equal([16, 33, 50])
+    expect(quickSort(collection.topK)).to.deep.equal([16, 33, 50])
     collection.add(18)
-    expect(heapSort(collection.topK)).to.deep.equal([18, 33, 50])
+    expect(quickSort(collection.topK)).to.deep.equal([18, 33, 50])
     collection.add(34)
-    expect(heapSort(collection.topK)).to.deep.equal([33, 34, 50])
+    expect(quickSort(collection.topK)).to.deep.equal([33, 34, 50])
     collection.add(58)
-    expect(heapSort(collection.topK)).to.deep.equal([34, 50, 58])
+    expect(quickSort(collection.topK)).to.deep.equal([34, 50, 58])
     collection.add(15)
-    expect(heapSort(collection.topK)).to.deep.equal([34, 50, 58])
+    expect(quickSort(collection.topK)).to.deep.equal([34, 50, 58])
     collection.add(17)
-    expect(heapSort(collection.topK)).to.deep.equal([34, 50, 58])
+    expect(quickSort(collection.topK)).to.deep.equal([34, 50, 58])
     collection.add(25)
-    expect(heapSort(collection.topK)).to.deep.equal([34, 50, 58])
+    expect(quickSort(collection.topK)).to.deep.equal([34, 50, 58])
     collection.add(51)
-    expect(heapSort(collection.topK)).to.deep.equal([50, 51, 58])
+    expect(quickSort(collection.topK)).to.deep.equal([50, 51, 58])
     collection.add(66)
-    expect(heapSort(collection.topK)).to.deep.equal([51, 58, 66])
+    expect(quickSort(collection.topK)).to.deep.equal([51, 58, 66])
     collection.add(19)
-    expect(heapSort(collection.topK)).to.deep.equal([51, 58, 66])
+    expect(quickSort(collection.topK)).to.deep.equal([51, 58, 66])
   })
 })
